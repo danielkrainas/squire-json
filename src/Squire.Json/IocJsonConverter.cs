c24791c574ba9883f846a395a7ff97ff11e5807d
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-03-27 09:44:42 -0400

    initial project load.

---
+++ src/Squire.Json/IocJsonConverter.cs
@@ -0,0 +1,50 @@
+﻿namespace Incant.Json
+{
+    using Microsoft.Practices.ServiceLocation;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Incant.Validation;
+
+    public class IocJsonConverter : JsonConverter
+    {
+        private readonly IServiceLocator locator;
+
+        public IocJsonConverter(IServiceLocator locator)
+        {
+            locator.VerifyParam("locator").IsNotNull();
+            this.locator = locator;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            if(!objectType.IsClass)
+            {
+                return false;
+            }
+
+            return this.locator.GetAllInstances<JsonConverter>().Any(c => c.CanConvert(objectType));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            foreach (var converter in this.locator.GetAllInstances<JsonConverter>().Where(c => c.CanConvert(objectType)))
+            {
+                existingValue = converter.ReadJson(reader, objectType, existingValue, serializer);
+            }
+
+            return existingValue;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            foreach (var converter in this.locator.GetAllInstances<JsonConverter>().Where(c => value == null || c.CanConvert(value.GetType())))
+            {
+                converter.WriteJson(writer, value, serializer);
+            }
+        }
+    }
+}
---
+++ src/Squire.Json/JsonHelpers.cs
@@ -0,0 +1,25 @@
+﻿namespace Incant.Json
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Incant.Validation;
+    using Incant.Unhinged;
+    using Microsoft.Practices.ServiceLocation;
+
+    public static class JsonHelpers
+    {
+        public static void AddLocator(this IList<JsonConverter> converters, IServiceLocator locator)
+        {
+            converters.VerifyParam("converters").IsNotNull();
+            locator.VerifyParam("locator").IsNotNull();
+            if (!converters.Any(c => c is IocJsonConverter))
+            {
+                converters.Add(new IocJsonConverter(locator));
+            }
+        }
+    }
+}
